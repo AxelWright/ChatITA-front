fca400b4dff645164c42e26394e81de144da1843
Axel Ramos <anotherman16@gmail.com>
2024-05-16 00:49:15 -0600

    Update SettingsScreen.tsx

--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -1,9 +1,7 @@
-// Importing components from 'react-native' and custom hooks and components
 import { SafeAreaView } from "react-native";
 import { useAuth } from "../../hooks";
 import { UserInfo, Options } from "../../components/Settings";
 
-// SettingsScreen functional component definition
 export function SettingsScreen() {
   // Destructuring properties from the useAuth hook
   const { user, accessToken, logout, updateUser } = useAuth();
